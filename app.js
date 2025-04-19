7cc6b68d0b4958199bf647fb389e1d12dfc1c929
BeneMee <benedict.meeske@gmail.com>
2025-04-19 16:37:52 +0200

    gamelogic of part color recognition and html and c and css structure

---
+++ app.js
@@ -0,0 +1,85 @@
+//globale Variabeln
+let baseColor;
+let AllItems;
+let correct_item_ID;
+
+// basisfarbe erzeugen random
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    baseColor = `rgb(${r}, ${g}, ${b})`; //backticks `` für template literal syntax
+    return baseColor;
+}
+
+//Farbe auf alle 6 Kacheln anwenden 
+function applyColorToTiles() {
+    /** @type {NodeListOf<HTMLElement>} */  
+    AllItems = document.querySelectorAll(".item")
+    baseColor = getRandomColor()
+
+    AllItems.forEach(function(item) {
+        item.style.backgroundColor = baseColor;
+    });
+}
+
+//slightly andere Farbe erzeugen 
+function getSlightlySimilarColor() {
+    let rgb_digits = baseColor.match(/\d+/g).map(Number);
+    let index = Math.floor(Math.random() * 3)
+    let veränderung = Math.random() < 0.5 ? -30 : 30;
+    rgb_digits[index] = Math.min(255, Math.max(0, rgb_digits[index] + veränderung))
+    // keine werte > 0 > 255 möglich!
+    let r = rgb_digits[0];
+    let g = rgb_digits[1];
+    let b = rgb_digits[2];
+    let slightlySimilarColor = `rgb(${r}, ${g}, ${b})`
+    return slightlySimilarColor
+}
+
+//slightly andere farbe auf eine random tile stylen
+function applySimilarColor() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    let item_id = `item_${randomNumber}`
+    correct_item_ID = item_id
+    let chosen_item = document.getElementById(item_id)
+    chosen_item.style.backgroundColor = getSlightlySimilarColor()
+}
+
+//clickable machen die tiles und game logik 
+function addEventListenerToTiles () {
+    AllItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+            if (item.id === correct_item_ID) {
+                console.log("RICHTIG!!");
+                applyColorToTiles();
+                applySimilarColor();
+
+            } else {
+                console.log("FALSCH!!!");
+            }
+        });
+    });
+}
+
+
+applyColorToTiles()
+applySimilarColor()
+addEventListenerToTiles()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
